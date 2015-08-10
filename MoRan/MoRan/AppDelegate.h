152fca71db0dc4f0a24d515a3ba58bfc0a0b2d39
ZukangWang <zukang89@hotmail.com>
2015-08-10 23:09:30 +0800

    蓦然空项目

---
+++ MoRan/MoRan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoRan
+//
+//  Created by 王祖康 on 15/8/10.
+//  Copyright (c) 2015年 GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoRan/MoRan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoRan
+//
+//  Created by 王祖康 on 15/8/10.
+//  Copyright (c) 2015年 GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
